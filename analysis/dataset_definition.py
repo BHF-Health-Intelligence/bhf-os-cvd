d1b8faaf0b038ea643fda80cb2e461309eda4462
Daniel O'Connell <daniel_oc@hotmail.co.uk>
2026-02-24 15:11:36 +0000

    Push new datatset and project yaml

--- analysis/dataset_definition.py
+++ analysis/dataset_definition.py
@@ -5,17 +5,13 @@ from ehrql.tables.tpp import emergency_care_attendances
 # Instantiation
 dataset = create_dataset()
 
-
 dataset.configure_dummy_data(population_size=1000)
 
 #CVD codes of-interest
 cvd_codes = codelist_from_csv("codelists/cvd-ae-cod.csv", column="code")
 
-#Diabetes codes - for testing
-#cvd_codes = codelist_from_csv("codelists/nhsd-primary-care-domain-refsets-dm_cod.csv", column="code")
-
 #Known sex:
-was_female_or_male = patients.sex.is_in(["female", "male"])
+is_female_or_male = patients.sex.is_in(["female", "male"])
 
 #Restrict events to those within date range
 date_start="2017-04-01"
@@ -25,7 +21,15 @@ date_end = "2025-05-01"
 age_at_start = patients.age_on(date_start)
 age_filter = (age_at_start >= 0) & (age_at_start <= 110)
 
+#Has diagnosis in codelist:
+codelist_filter=emergency_care_attendances.diagnosis_01.is_in(cvd_codes)
 
-show(emergency_care_attendances.where(emergency_care_attendances.diagnosis_01.is_in(cvd_codes)))
 
-dataset.define_population(age_filter)
+#Implement filters
+dataset.define_population(age_filter,
+                          is_female_or_male,
+                          )
+
+#Define columns
+dataset.age = age_at_start
+dataset.code = emergency_care_attendances[].diagnosis_01
