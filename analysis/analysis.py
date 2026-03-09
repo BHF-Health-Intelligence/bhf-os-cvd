f4c9f3f45754f343abb78fca660e4a8afe8b2958
Daniel O'Connell <daniel_oc@hotmail.co.uk>
2026-03-09 16:54:53 +0000

    Added dataset criteria

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -2,26 +2,10 @@ import pandas as pd
 import os 
 
 
-# this_dir=os.getcwd()
-
-# dataset_location=os.path.join(this_dir,"output", "dataset.csv.gz") 
-
-# print("You are in:", this_dir)
+#Read input data
+dataset_location=os.path.join("output","dataset.csv.gz") 
+df = pd.read_csv(dataset_location, compression='gzip')
 
 
-# # working_dir = os.getcwd()
-# print(f"Containing: {os.listdir(this_dir)}")
-
-
-
-# os.chdir()
-
-
-
-
-print(os.path.exists("./output/dataset.csv.gz"))
-
-
-
-# df = pd.read_csv(dataset_location, compression='gzip')
-# df.head().to_csv(os.path.join(this_dir, "output", "analysis_results.csv"), index=False)
+#Save to output 
+df.head().to_csv(os.path.join("output", "analysis_results.csv"), index=False)
--- analysis/dataset_definition.py
+++ analysis/dataset_definition.py
@@ -1,6 +1,11 @@
 from ehrql import codelist_from_csv, create_dataset, show
-from ehrql.tables.core import patients, practice_registrations, clinical_events, medications
-from ehrql.tables.tpp import emergency_care_attendances
+from ehrql.tables.core import patients
+from ehrql.tables.tpp import emergency_care_attendances, addresses
+
+
+#For reference on emergency_care (ECDS):
+#https://docs.opensafely.org/ehrql/reference/schemas/tpp/#emergency_care_attendances
+
 
 # Instantiation
 dataset = create_dataset()
@@ -22,14 +27,23 @@ age_at_start = patients.age_on(date_start)
 age_filter = (age_at_start >= 0) & (age_at_start <= 110)
 
 #Has diagnosis in codelist:
-codelist_filter=emergency_care_attendances.diagnosis_01.is_in(cvd_codes)
+# codelist_filter=emergency_care_attendances.diagnosis_01.is_in(cvd_codes)
+
+
+dataset.cvd_admission = emergency_care_attendances.where(
+    emergency_care_attendances.diagnosis_01.is_in(cvd_codes)
+).where(
+    emergency_care_attendances.arrival_date .is_on_or_between(date_start, date_end)
+).exists_for_patient()
+
 
 
 #Implement filters
-dataset.define_population(age_filter,
-                          is_female_or_male,
-                          )
+dataset.define_population(age_filter & 
+                          is_female_or_male )
 
 #Define columns
 dataset.age = age_at_start
-dataset.code = emergency_care_attendances[].diagnosis_01
+# dataset.code = codelist_filter
+# dataset.imd = addresses.for_patient_on(date_start).imd_rounded
+# dataset.ethnicity=emergency_care_attendances.ethnicity_from_sus
