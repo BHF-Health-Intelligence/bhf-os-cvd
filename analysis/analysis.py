24c2fdb42f01c6a3bdb82ed5e8869485ec9c4fb8
Daniel O'Connell <daniel_oc@hotmail.co.uk>
2026-03-09 17:17:09 +0000

    Updated

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -8,15 +8,15 @@ print("Starting analysis...")
 print("Current working directory:", os.getcwd())
 
 
-for item in os.listdir('output'):
-    print(item)
+# for item in os.listdir('output'):
+#     print(item)
 
 
 
-# Read input data
-dataset_location=os.path.join("analysis","dataset.csv.gz") 
-df = pd.read_csv(dataset_location, compression='gzip')
+# # Read input data
+# dataset_location=os.path.join("analysis","dataset.csv.gz") 
+# df = pd.read_csv(dataset_location, compression='gzip')
 
 
-# Save to output 
-df.head().to_csv(os.path.join("output", "analysis_results.csv"), index=False)
+# # Save to output 
+# df.head().to_csv(os.path.join("output", "analysis_results.csv"), index=False)
