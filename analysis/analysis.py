4fd0cf2ffd5074f1067994f08e1200b8d0137615
Daniel O'Connell <daniel_oc@hotmail.co.uk>
2026-03-10 14:18:47 +0000

    Updated

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -6,14 +6,14 @@ print("Starting analysis...")
 print("Current working directory:", os.getcwd())
 
 
-for item in os.listdir('analysis'):
+for item in os.listdir('dummy_tables'):
     print(item)
 
 
-# # Read input data
-# dataset_location=os.path.join("analysis","dataset.csv.gz") 
-# df = pd.read_csv(dataset_location, compression='gzip')
+# Read input data
+dataset_location=os.path.join("dummy_tables","dataset.csv.gz") 
+df = pd.read_csv(dataset_location, compression='gzip')
 
 
-# # Save to output 
-# df.head().to_csv(os.path.join("output", "analysis_results.csv"), index=False)
+# Save to output 
+df.head().to_csv(os.path.join("output", "analysis_results.csv"), index=False)
